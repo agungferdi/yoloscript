1f905ed631337f05dcd3c6f9b89d9e1af7dd7107
Muhammad Agung Ferdiansyah <muhammad_agungf@student.ub.ac.id>
2023-10-01 06:34:22 +0700

    Initial commit

--- testdeployments/predictor.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-import sys
-
-file = input('Enter file to predict on: ')
-
-model = YOLO('../models_train/97epoch.pt')
-model.predict(source=str(file), save= True)
--- webapp.py
+++ webapp.py
@@ -1,8 +1,6 @@
 import os
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
-from PIL import Image
-import numpy as np
 import cv2
 from ultralytics import YOLO
 from base64 import b64encode
@@ -11,91 +9,134 @@ from collections import defaultdict
 import json
 import webcolors
 
-model = YOLO('models_train/97epoch.pt') # instantiate the model
-UPLOAD_FOLDER = 'upload_folder' # define were files will be saved
-os.makedirs(UPLOAD_FOLDER, exist_ok=True) # create directory for save if none there
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'} # define allowed files
-app = Flask(__name__) #instatiate flask application
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER # set upload folder
-def allowed_file(filename): # check if uploaded file has an allowed extension
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-@app.route('/', methods=['GET', 'POST']) #define GET and POST methods
+model = YOLO('best.pt')
+UPLOAD_FOLDER = 'upload_folder'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+cumulative_count = 0
+
+@app.route('/', methods=['GET', 'POST'])
 def upload_file():
+    global cumulative_count
+
     if request.method == 'POST':
-        if 'file' not in request.files: # if no file is submitted
+        if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        if file and allowed_file(file.filename): # if there is a file and it's allowed continue
-            filename = secure_filename(file.filename) # ensure safe file
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))# save file
-            # img = Image.open(f'{UPLOAD_FOLDER}/{filename}')
-            img = cv2.imread(f'{UPLOAD_FOLDER}/{filename}') # open file
-            output_img, classes, colorHex = inference_img(img) # perform inference
-            _, buffer = cv2.imencode('.jpg', output_img) #encode image from jpg
-            b64_img = b64encode(buffer).decode() # encode butter file for html display
-            ## HTML for post
-            return f''' 
-            <!doctype html>
-            <h1> Your Agar Plate with Detections </h1>
-            <img src=data:image/jpeg;base64,{b64_img} width="960" height="960">
-            <h3> Colonies per class </h3>
-            <p> {json.dumps(classes)} </p>
-            <h3> LEGEND </h3>
-            <p style= "color:{colorHex['S.aureus']};">S.aureus</p>
-            <p style= "color:{colorHex['B.subtilis']};">B.subtilis</p>
-            <p style= "color:{colorHex['P.aeruginosa']};">P.aeruginosa</p>
-            <p style= "color:{colorHex['E.coli ']};">E.coli</p>
-            <p style= "color:{colorHex['C.albicans']};">C.albicans</p>
-            <form method="GET" action="/">  
-                <input Home type="submit" value="Test another image"/>  
-            </form>    
-            </form>
-            '''
-    else:
-        return '''
-        <!doctype html>
-        <title>Upload new File</title>
-        <h1>Upload new File</h1>
-        <form method=post enctype=multipart/form-data>
-        <input type=file name=file>
-        <input type=submit value=Upload>
-        </form>
-        '''
-def inference_img(img: np.array) -> np.array: # take in image as numpy array and return a numpy array
+
+        if 'webcam_image_data' in request.files:
+
+            file = request.files['webcam_image_data']
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+
+
+            img = cv2.imread(file_path)
+
+            if img is None:
+                flash('Error: Failed to load the image')
+                return redirect(request.url)
+
+            output_img, classes, colorHex = inference_img(img)
+            _, buffer = cv2.imencode('.jpg', output_img)
+            b64_img = b64encode(buffer).decode()
+
+            cumulative_count += classes["Total Corn"]
+
+            return render_template('result.html', b64_img=b64_img, classes=classes, cumulative_count=cumulative_count)
+
+    return render_template('upload.html')
+
+def inference_img(img):
     imgsize = 1280
-    results = model.predict(img, imgsz=imgsize) #define image size
-    output_img = img.copy() # copy image for bbox drawing
-    # define classes
+    results = model.predict(img, imgsz=imgsize)
+    output_img = img.copy()
     class_maps = [
-        "S.aureus",
-        "B.subtilis", 
-        "P.aeruginosa",
-        "E.coli ",
-        "C.albicans",
+        "Total Corn",
     ]
-    colorsRGB = matplotlib.cm.tab20(range(len(class_maps))) # import colors
-    colors = [(i[:-1][::-1]*255) for i in colorsRGB] # convert colors to portion of 255 in BGR format
-    colorsRev = [(i[:-1][::1]*255) for i in colorsRGB] # convert colors to portion of 255 in RGB format
-    colorsTuple = [(int(x),int(y),int(z)) for x,y,z in colorsRev] # convert form list to tuple
-    colorHex = {x:webcolors.rgb_to_hex(y) for x, y in zip(class_maps, colorsTuple)} # convert RBG to hex code
-    print(colors)
-    classes_found = defaultdict(int) # instaiate dictionary with found colonies
-    for result in results: # take bboxes and draw them on to image
+    colorsRGB = matplotlib.cm.tab20(range(len(class_maps)))
+    colors = [(i[:-1][::-1] * 255) for i in colorsRGB]
+    colorsRev = [(i[:-1][::1] * 255) for i in colorsRGB]
+    colorsTuple = [(int(x), int(y), int(z)) for x, y, z in colorsRev]
+    colorHex = {x: webcolors.rgb_to_hex(y) for x, y in zip(class_maps, colorsTuple)}
+    classes_found = defaultdict(int)
+    for result in results:
         boxes = result.boxes.to('cpu').numpy()
         classes = boxes.cls.astype(int)
         for box, cls in zip(boxes, classes):
             bbox_class = class_maps[cls]
-            coord = box.xyxy.astype(int).squeeze() # return bbox coord in smallest format
+            coord = box.xyxy.astype(int).squeeze()
             xmin, ymin, xmax, ymax = coord
-            classes_found[bbox_class] += 1 # count colonies
-            
-            color = colors[cls] # define color by class
-            color = tuple(color) # convert color to a tuple
+            classes_found[bbox_class] += 1
+            color = colors[cls]
+            color = tuple(color)
+            cv2.rectangle(output_img, (xmin, ymin), (xmax, ymax), color, 2)
+    return output_img, classes_found, colorHex
 
-            cv2.rectangle(output_img, (xmin, ymin), (xmax, ymax), color, 2) # draw rectangle on image
-    print(classes_found)
-    return output_img, classes_found, colorHex # return image with bboxes, colony count, and hex values for colors used
-if __name__ == '__main__': # run application
+@app.route('/reset', methods=['POST'])
+def reset_count():
+    global cumulative_count
+    cumulative_count = 0
+    return redirect('/')
+@app.route('/capture_webcam', methods=['POST'])
+def capture_webcam(camera=None):
+    global cumulative_count
+
+    try:
+
+        if 'camera' in globals():
+            camera.release()
+
+
+        camera = cv2.VideoCapture(0)
+
+
+        if not camera.isOpened():
+            print("Error: Could not open webcam")
+            return redirect('/')
+
+
+        desired_exposure_value = 0.1
+
+
+        camera.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
+        camera.set(cv2.CAP_PROP_EXPOSURE, 0.2 )
+
+        ret, frame = camera.read()
+
+        if ret:
+            filename = 'webcam_capture.jpg'
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            cv2.imwrite(file_path, frame)
+
+
+            camera.release()
+
+            img = cv2.imread(file_path)
+            output_img, classes, colorHex = inference_img(img)
+            _, buffer = cv2.imencode('.jpg', output_img)
+            b64_img = b64encode(buffer).decode()
+
+            cumulative_count += classes["Total Corn"]
+
+            return render_template('result.html', b64_img=b64_img, classes=classes, cumulative_count=cumulative_count)
+
+    except Exception as e:
+        print("Error capturing from webcam:", str(e))
+
+    return redirect('/')
+
+
+if __name__ == '__main__':
     app.run()
